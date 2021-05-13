6342c55c135871906759ba7f1c387b9a0e663a72
周宇盛 Yusheng Zhou <jasonzhouu@outlook.com>
2021-05-13 21:16:50 +0800

    delete store

--- store/index.ts
+++ store/index.ts
@@ -2,20 +2,7 @@ import { createStore } from './createSore'
 import { useLocalStore } from 'mobx-react-lite'
 
 export function useModel() {
-  return useLocalStore(() => ({
-    theme: 'dark',
-    setTheme(value: 'dark' | 'light') {
-      this.theme = value
-    },
-    anchor1Appear: false,
-    setAnchor1Appear(value: boolean) {
-      this.anchor1Appear = value
-    },
-    anchor2Appear: false,
-    setAnchor2Appear(value: boolean) {
-      this.anchor2Appear = value
-    }
-  }))
+  return useLocalStore(() => ({}))
 }
 
 const store = createStore(useModel)
