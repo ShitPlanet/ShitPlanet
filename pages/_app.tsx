1c375b6e99bf526df49fef794b1160dd461b0778
AmberHu <amoslover@163.com>
2021-05-13 23:33:11 +0800

    add lottie and sass

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from 'react'
-import '@/global.css'
+import '@/global.scss'
 import { observer } from 'mobx-react-lite'
 import { Provider } from '@/store'
 import Head from 'next/head'
